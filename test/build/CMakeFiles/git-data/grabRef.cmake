99d97bea411776da92a84af42029340fd56d28d0
odddouglas <18718048671iota@gmail.com>
2025-07-11 20:51:05 +0800

    build: 构建espidf基本框架

--- test/build/CMakeFiles/git-data/grabRef.cmake
+++ test/build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "E:/ODDDOUGLAS/mini-project/project/impedance-measurer/test/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "E:/ODDDOUGLAS/maxi-file/esp32/ESP-IDF/esp/v5.3.2/esp-idf/.git")
+set(GIT_DIR "E:/ODDDOUGLAS/mini-project/project/impedance-measurer/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
